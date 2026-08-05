a4d012a35c7b1155e3b0f4abaebd46aa45bfb19a
emmanuel <ntimemma23@gmail.com>
2026-08-05 23:41:54 +0000

    Remove old project files

--- app/models/portfolio.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-
-class Portfolio(SQLModel, table=True):
-    """An electoral position, e.g. 'President', 'Secretary'."""
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str = Field(unique=True, index=True)
